67798d82443d4dd82e4a275640de0cabb621da76
andersonsantanalima <n353347@gmail.com>
2023-10-03 08:26:34 -0300

    Ambiente preparado

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Parse from "parse/react-native.js";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+Parse.setAsyncStorage(AsyncStorage);
+
+Parse.initialize('iViRHL3CzKeIsxpNjBsN3K8VoYLtAmniZYQ590Y8','7xDsT1Fpv2YipYceX58PLQrkTmyZZ9BM3NkJOEk2');
+Parse.serverURL = 'https://parseapi.back4app.com/';
 
 export default function App() {
   return (
